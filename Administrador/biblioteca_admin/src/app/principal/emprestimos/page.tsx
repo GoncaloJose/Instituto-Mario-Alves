ccc63e99422000ce092a82c381435dad6841e236
Ju Braga <jubragaads@gmail.com>
2025-05-21 18:38:48 -0300

    Ajuste data do emprestimo

--- Administrador/biblioteca_admin/src/app/principal/emprestimos/page.tsx
+++ Administrador/biblioteca_admin/src/app/principal/emprestimos/page.tsx
@@ -10,18 +10,20 @@ interface Usuario {
 
 interface Livro {
   id: number;
-  nome: string;
+  titulo: string;
 }
 
 type Inputs = {
   usuarioId: number;
   livroId: number;
+  dataRetirada: string;
+  dataEntrega: string;
 };
 
 function Emprestimos() {
   const [usuarios, setUsuarios] = useState<Usuario[]>([]);
   const [livros, setLivros] = useState<Livro[]>([]);
-  const { register, handleSubmit, setFocus } = useForm<Inputs>();
+  const { register, handleSubmit, setFocus, setValue } = useForm<Inputs>();
 
   useEffect(() => {
     async function fetchData(endpoint: string, setData: Function) {
@@ -37,6 +39,16 @@ function Emprestimos() {
     fetchData("livros", setLivros);
 
     setFocus("usuarioId");
+
+    // Definir datas automaticamente
+    const hoje = new Date();
+    const retirada = hoje.toISOString().split("T")[0];
+    const entrega = new Date(hoje);
+    entrega.setDate(hoje.getDate() + 7);
+    const entregaFormatada = entrega.toISOString().split("T")[0];
+
+    setValue("dataRetirada", retirada);
+    setValue("dataEntrega", entregaFormatada);
   }, []);
 
   async function realizarEmprestimo(data: Inputs) {
@@ -77,9 +89,35 @@ function Emprestimos() {
           <select id="livroId" className="block border border-gray-500 rounded-md p-2 text-black" {...register("livroId")}>
             <option value="">Selecione um livro</option>
             {livros.map(livro => (
-              <option key={livro.id} value={livro.id}>{livro.nome}</option>
+              <option key={livro.id} value={livro.id}>{livro.titulo}</option>
             ))}
           </select>
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="dataRetirada" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Data de Retirada
+          </label>
+          <input
+            type="date"
+            id="dataRetirada"
+            className="block border border-gray-500 rounded-md p-2"
+            {...register("dataRetirada")}
+            readOnly
+          />
+        </div>
+
+        <div className="mb-3">
+          <label htmlFor="dataEntrega" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+            Data de Entrega
+          </label>
+          <input
+            type="date"
+            id="dataEntrega"
+            className="block border border-gray-500 rounded-md p-2"
+            {...register("dataEntrega")}
+            readOnly
+          />
         </div>
 
         <button type="submit" className="bg-vermelho hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
