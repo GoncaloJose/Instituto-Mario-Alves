d79cba963d93681e9da330fb8750c00a54a65a46
Ju Braga <jubragaads@gmail.com>
2025-06-14 17:48:02 -0300

    mostrar senha
    mostrar senha do adm

--- Administrador/biblioteca_admin/src/app/page.tsx
+++ Administrador/biblioteca_admin/src/app/page.tsx
@@ -1,10 +1,11 @@
 "use client";
-import { useEffect } from "react";
+
+import { useEffect, useState } from "react";
 import { useForm } from "react-hook-form";
 import { toast } from "sonner";
 import { useRouter } from "next/navigation";
-
 import Cookies from "js-cookie";
+import { Eye, EyeOff } from "lucide-react"; // ícones
 
 type Inputs = {
   email: string;
@@ -14,10 +15,11 @@ type Inputs = {
 export default function Home() {
   const { register, handleSubmit, setFocus } = useForm<Inputs>();
   const router = useRouter();
+  const [mostrarSenha, setMostrarSenha] = useState(false);
 
   useEffect(() => {
     setFocus("email");
-  }, []);
+  }, [setFocus]);
 
   async function verificaLogin(data: Inputs) {
     const response = await fetch("http://localhost:3004/usuarios/login", {
@@ -26,27 +28,22 @@ export default function Home() {
       body: JSON.stringify({ email: data.email, senha: data.senha }),
     });
 
-    // console.log(response)
-    if (response.status == 200) {
-      
+    if (response.status === 200) {
       const usuario = await response.json();
 
-      if ( usuario.admin ) {
-
+      if (usuario.admin) {
         Cookies.set("admin_logado_id", usuario.id);
         Cookies.set("admin_logado_nome", usuario.nome);
         Cookies.set("admin_logado_token", usuario.token);
-
         router.push("/principal");
-        
-      } else { 
-          toast.error("Erro...Acesso Restrito!!");
+      } else {
+        toast.error("Erro... Acesso Restrito!!");
       }
-
-    } else if (response.status == 400) {
+    } else if (response.status === 400) {
       toast.error("Erro... Login ou senha incorretos");
     }
   }
+
   return (
     <main className="max-w-screen-xl flex flex-col items-center mx-auto p-6">
       <img
@@ -55,13 +52,10 @@ export default function Home() {
         style={{ width: 240 }}
         className="d-block"
       />
-      <div className="max-w-sm">
+      <div className="max-w-sm w-full">
         <h1 className="text-3xl font-bold my-8">Administrador: Subversivo</h1>
-        <form
-          className="max-w-sm mx-auto"
-          onSubmit={handleSubmit(verificaLogin)}
-        >
-          <div className="mb-5">
+        <form onSubmit={handleSubmit(verificaLogin)} className="space-y-6">
+          <div>
             <label
               htmlFor="email"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -71,12 +65,13 @@ export default function Home() {
             <input
               type="email"
               id="email"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
               {...register("email")}
               required
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
             />
           </div>
-          <div className="mb-5">
+
+          <div className="relative">
             <label
               htmlFor="password"
               className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
@@ -84,16 +79,24 @@ export default function Home() {
               Senha:
             </label>
             <input
-              type="password"
+              type={mostrarSenha ? "text" : "password"}
               id="password"
-              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
               {...register("senha")}
               required
+              className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 pr-10 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white"
             />
+            <button
+              type="button"
+              onClick={() => setMostrarSenha((prev) => !prev)}
+              className="absolute right-3 top-9 transform -translate-y-1/2 text-gray-600 dark:text-gray-300"
+            >
+              {mostrarSenha ? <EyeOff size={20} className="mt-7"/> : <Eye size={20} className="mt-7"/>}
+            </button>
           </div>
+
           <button
             type="submit"
-            className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full sm:w-auto px-5 py-2.5 text-center dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
+            className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:outline-none focus:ring-red-300 font-medium rounded-lg text-sm w-full px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 dark:focus:ring-red-800"
           >
             Entrar
           </button>
