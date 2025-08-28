eef90746eae9f0a1323675504e7efb6cde4486d5
Ju Braga <jubragaads@gmail.com>
2025-08-28 17:22:46 -0300

    layout ADM

--- Administrador/biblioteca_admin/src/app/principal/usuarios/page.tsx
+++ Administrador/biblioteca_admin/src/app/principal/usuarios/page.tsx
@@ -34,14 +34,14 @@ function CadUsuarios() {
           Controle de Usuários
         </h1>
         <Link href="usuarios/novo" 
-          className="text-black bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-300 font-bold rounded-lg text-md px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">
+          className="text-white bg-vermelho hover:bg-vermelho focus:ring-4 focus:ring-red-300 font-bold rounded-lg text-md px-5 py-2.5 me-2 mb-2 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800">
           Novo Usuário
         </Link>
       </div>
 
       <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
         <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
-          <thead className="text-xl text-gray-700 uppercase bg-red-500 dark:bg-gray-700 dark:text-gray-400">
+          <thead className="text-xl text-white uppercase bg-vermelho dark:bg-gray-700 dark:text-gray-400">
             <tr>
               <th scope="col" className="px-6 py-3">
                 Nome
