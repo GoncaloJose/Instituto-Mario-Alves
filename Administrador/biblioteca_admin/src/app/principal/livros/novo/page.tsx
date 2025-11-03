8fe5f841f59eb23d31a939167e9b4e2713a7f778
GoncaloJose <fgoncalojose@gmail.com>
2025-11-03 20:06:13 -0300

    Corrigindo livros para usar text area

--- Administrador/biblioteca_admin/src/app/principal/livros/novo/page.tsx
+++ Administrador/biblioteca_admin/src/app/principal/livros/novo/page.tsx
@@ -204,12 +204,12 @@ function NovoLivro() {
           <label htmlFor="sinopse" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
             Sinopse
           </label>
-          <input
-            type="text"
+          <textarea
             id="sinopse"
             className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-red-500 focus:border-red-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-red-500 dark:focus:border-red-500"
             {...register("sinopse")}
-          />
+          >
+		  </textarea>
         </div>
 
         {/* --- CAMPO FOTO --- (sem mudança) */}
