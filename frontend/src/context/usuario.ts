af6757f7ea55b3b6586c166478e132dc4f8f1155
GoncaloJose <fgoncalojose@gmail.com>
2025-11-04 01:19:06 -0300

    Busca e retoma usuários do localStorage

--- frontend/src/context/usuario.ts
+++ frontend/src/context/usuario.ts
@@ -1,14 +1,22 @@
 import { UsuarioI } from '@/utils/types/usuarios';
 import { create } from 'zustand';
+import { persist, createJSONStorage } from 'zustand/middleware'
 
 type UsuarioStore = {
     usuario: UsuarioI;
     logaUsuario: (usuarioLogado: UsuarioI) => void;
-    deslogaUsuario: () => void; // Corrigido o nome para "deslogaUsuario"
+    atualizaUsuario: (usuarioAtualizado: UsuarioI) => void;
+    deslogaUsuario: () => void;
 };
 
-export const useUsuarioStore = create<UsuarioStore>((set) => ({
-    usuario: {} as UsuarioI,
-    logaUsuario: (usuarioLogado) => set({ usuario: usuarioLogado }),
-    deslogaUsuario: () => set({ usuario: {} as UsuarioI }) // Nome unificado
-}));
+export const useUsuarioStore = create<UsuarioStore>(
+    persist((set, get) => ({
+        usuario: {} as UsuarioI,
+        logaUsuario: (usuarioLogado) => set({ usuario: usuarioLogado }),
+        atualizaUsuario: (usuarioAtualizado: UsuarioI) => set({ usuario: usuarioAtualizado }),
+        deslogaUsuario: () => set({ usuario: {} as UsuarioI }) // Nome unificado
+    }),
+    {
+        name: 'ima-storage',
+    }
+));
