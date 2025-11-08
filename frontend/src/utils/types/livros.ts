3cd737020b77a5ceb0049c68e1f8cee81558a00e
GoncaloJose <fgoncalojose@gmail.com>
2025-11-08 03:23:39 -0300

    Ajustando type do livros
    Co-Authored-By: Ju Braga <134513643+JuBragaADS@users.noreply.github.com>

--- frontend/src/utils/types/livros.ts
+++ frontend/src/utils/types/livros.ts
@@ -7,6 +7,10 @@ export interface LivroI {
     genero:           string
     editora:          string
     autor:            string
+    autores:         Array<{ id: number; nome: string }>
+    editoras:        { id: number; nome: string }
+    generos:         Array<{ id: number; tipo: string }>
+    destaque:         boolean
     createdAt:        Date
     updatedAt:        Date
 }
