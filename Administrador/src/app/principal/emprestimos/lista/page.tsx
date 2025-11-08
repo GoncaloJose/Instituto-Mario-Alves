22bfa78b2aff7b65a4e60314e8d8dc3f357e5e03
GoncaloJose <fgoncalojose@gmail.com>
2025-11-08 01:02:04 -0300

    Lista de emprestimos e retorno
    Co-Authored-By: Ju Braga <134513643+JuBragaADS@users.noreply.github.com>

--- Administrador/src/app/principal/emprestimos/lista/page.tsx
+++ Administrador/src/app/principal/emprestimos/lista/page.tsx
@@ -1,75 +1,92 @@
 "use client";
 import React from "react";
 import { useState, useEffect } from "react";
-import { EmprestimoI } from "@/utils/types/emprestimos"; // Importe seu tipo
-import ItemEmprestimo from "@/components/ItemEmprestimo"; // Importe seu componente de item
+import { EmprestimoI } from "@/utils/types/emprestimos";
+import ItemEmprestimo from "@/components/ItemEmprestimo";
 import Link from "next/link";
+import { toast } from 'sonner';
 
-// Esta é a nova página que só mostra a lista
 function ListaDeEmprestimos() {
   const [emprestimos, setEmprestimos] = useState<EmprestimoI[]>([]);
-  const [carregando, setCarregando] = useState(true); // Começa como 'true' // useEffect busca os empréstimos quando a página carrega
+  const [carregando, setCarregando] = useState(true);
+
+  async function getEmprestimos() {
+    setCarregando(true);
+
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_URL_API}/emprestimos`
+      );
+      const dados = await response.json();
+      setEmprestimos(dados);
+    } catch (error) {
+      console.error("Erro ao buscar empréstimos:", error);
+    }
+
+    setCarregando(false);
+  }
 
   useEffect(() => {
-    async function getEmprestimos() {
-      setCarregando(true);
-      try {
-        const response = await fetch(
-          `${process.env.NEXT_PUBLIC_URL_API}/emprestimos`
-        );
-        const dados = await response.json();
-        setEmprestimos(dados);
-      } catch (error) {
-        console.error("Erro ao buscar empréstimos:", error);
-      }
-      setCarregando(false);
-    }
     getEmprestimos();
-  }, []); // Roda apenas uma vez // Mapeia os empréstimos para o componente ItemEmprestimo
+  }, []);
 
-  const ItemComp = ItemEmprestimo as unknown as React.ComponentType<any>;
+  const onEntregar = async (id) => {
+    try {
+      const response = await fetch(
+        `${process.env.NEXT_PUBLIC_URL_API}/emprestimos/${id}/retorno`
+      );
+
+      if (response.ok) {
+        getEmprestimos()
+      }
+    } catch (error) {
+      console.error("Erro ao buscar empréstimos:", error);
+    } finally {
+      toast.success('Livro foi marcado como retornado');
+    }
+  }
+
   const listaEmprestimos = emprestimos.map((emprestimo: EmprestimoI) => (
-    <ItemComp
+    <ItemEmprestimo
       key={emprestimo.id}
       emprestimo={emprestimo}
-      emprestimos={emprestimos}
-      setEmprestimos={setEmprestimos}
+      onEntregue={onEntregar}
     />
   ));
 
   return (
-    <div className="m-4 mt-24">
-           
-      <div className="flex justify-between items-center mb-6">
-               
-        <h1 className="text-3xl font-bold text-gray-900 dark:text-white">
-                    Lista de Empréstimos        
-        </h1>
-        {/* Este link volta para a página do formulário */}       
+    <div className='m-4 mt-24'>
+      <div className="flex">
         <Link
-          href="/principal/emprestimos"
-          className="text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-red-500 font-bold rounded-lg text-md px-5 py-2.5 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800"
+        href="/principal/emprestimos"
+        className="text-gray-400 hover:text-red-700 focus:ring-4 focus:ring-red-500 font-bold rounded-lg text-md py-2.5 dark:bg-red-600 dark:hover:bg-red-700 focus:outline-none dark:focus:ring-red-800"
         >
-                    ← Voltar ao Cadastro        
+          ← Voltar ao Cadastro        
         </Link>
-             
       </div>
-           
-      <div className="mt-4">
-               
-        {carregando ? (
-          <p>Carregando...</p>
-        ) : (
-          <div className="border rounded-lg dark:border-gray-700 overflow-hidden">
-                       {" "}
-            {listaEmprestimos.length > 0 ? (
-              listaEmprestimos
-            ) : (
-              <p className="p-4">Nenhum empréstimo encontrado.</p>
-            )}
-                     
-          </div>
-        )}
+      <div className='flex justify-between items-center mb-4'>
+        <h1 className="text-2xl font-bold leading-none tracking-tight text-gray-900 md:text-3xl lg:text-4xl dark:text-white">
+          Controle de Emprestimos
+        </h1>
+      </div>
+
+      <div className="relative overflow-x-auto shadow-md sm:rounded-lg">
+            {(emprestimos.length && (
+              <table className="w-full text-sm text-left rtl:text-right text-gray-500 dark:text-gray-400">
+                <thead className="text-xl text-white uppercase bg-vermelho dark:bg-gray-700 dark:text-gray-400">
+                  <tr>
+                    <th scope="col" className="px-6 py-3">Nome</th>
+                    <th scope="col" className="px-6 py-3">Livro</th>
+                    <th scope="col" className="px-6 py-3">Autores</th>
+                    <th scope="col" className="px-6 py-3">Status</th>
+                    <th scope="col" className="px-6 py-3">Ações</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  {listaEmprestimos}
+                </tbody>
+              </table>
+            ))}
       </div>
     </div>
   );
